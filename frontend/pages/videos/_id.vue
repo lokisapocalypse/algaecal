41963fbc713f52af0607ef4331c38d054299e9c8
Brian Lamb <fusani.applications@gmail.com>
2020-08-07 21:36:01 -0400

    Add video player component

--- frontend/pages/videos/_id.vue
+++ frontend/pages/videos/_id.vue
@@ -1,28 +1,17 @@
 <template>
     <div class="container-fluid">
-        <iframe :src="videoLink" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="620" height="349"></iframe>
-        <div>{{ this.video.description }}</div>
+      <VideoPlayer :id="this.$route.params.id" />
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 
+import VideoPlayer from '@/components/VideoPlayer.vue';
+
 export default {
-  computed: {
-    videoLink() {
-      return '//fast.wistia.net/embed/iframe/' + this.video.video_id;
-    }
-  },
-  created() {
-    axios.get('http://localhost:8080/videos/' + this.$route.params.id).then(response => {
-      this.video = response.data.data;
-    });
-  },
-  data() {
-    return {
-      video: {}
-    };
-  }
+  components: [
+    VideoPlayer
+  ]
 };
 </script>
